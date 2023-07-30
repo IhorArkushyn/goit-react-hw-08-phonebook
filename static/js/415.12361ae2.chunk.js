ac3307fa2cda11cf4bdfdde0a2f216d0690bb53a
IhorArkushyn <114363817+IhorArkushyn@users.noreply.github.com>
2023-07-30 18:16:23 +0000

    Deploying to gh-pages from @ IhorArkushyn/goit-react-hw-08-phonebook@ed3a28ec34cb4e20901f96c7526782d684cf0210 🚀

---
+++ static/js/415.12361ae2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var o=t(4554),r=t(184);function c(){return(0,r.jsx)(o.Z,{sx:{display:"flex",justifyContent:"center",pt:10},children:(0,r.jsx)("h1",{children:"Phone book manager welcome page"})})}}}]);
+//# sourceMappingURL=415.12361ae2.chunk.js.map
